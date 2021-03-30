6da37b6e868e650575f339b130a3afbbcc73edd9
David <dvaquero@bilbomatica.es>
2021-03-30 11:16:58 +0200

    correcciones de cobertura de código

--- src/test/java/com/cursosdedesarrollo/microserviciospring/MicroservicioSpringApplicationTests.java
+++ src/test/java/com/cursosdedesarrollo/microserviciospring/MicroservicioSpringApplicationTests.java
@@ -1,7 +1,9 @@
 package com.cursosdedesarrollo.microserviciospring;
 
+import com.cursosdedesarrollo.microserviciospring.repository.PokemonRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -10,10 +12,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MicroservicioSpringApplicationTests {
+    @Autowired
+    public PokemonRepository pokemonRepository;
 
     @Test
     public void contextLoads() {
         assertThat(true).isNotNull();
+        MicroservicioSpringApplication.loadCSVData(pokemonRepository);
+        assertThat(pokemonRepository.count()).isEqualTo(721);
     }
 
 }
